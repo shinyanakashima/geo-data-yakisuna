437ac388553677863c5b6c3a8edc6cb79606938e
shinyanakashima <shinya.gp@gmail.com>
2025-11-18 00:00:31 +0900

    ポイントが間引かれないように対応

--- conv.sh
+++ conv.sh
@@ -3,6 +3,14 @@
 tippecanoe \
   -o yakisuna.pmtiles \
   --read-parallel \
+  -r1 \
   --force \
-  $(for f in yakisuna_*.ndjson; do echo --layer=${f%.ndjson} $f; done)
+  --no-feature-limit \
+  --no-tile-size-limit \
+  --minimum-zoom=4 \
+  --maximum-zoom=20 \
+  $(for f in yakisuna_*.ndjson; do
+      city=$(echo $f | sed 's/yakisuna_//' | sed 's/[0-9]*\.ndjson//')
+      echo --named-layer=${city}:$f
+    done)
 
